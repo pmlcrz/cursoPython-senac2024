31897d193ad7dcc7b355de753fb53af49ebaf4e0
Pamela Cruz 🖳 <117556345+pmlcrz@users.noreply.github.com>
2024-07-17 00:14:17 -0300

    Add files via upload

---
+++ Modulo_1/if.py
@@ -0,0 +1,18 @@
+ # Verificar se é maior de idade
+
+maiorIdade= 18
+idade = int (input('Digite sua idade\n'))
+
+if (idade >= maiorIdade):
+    a=1
+    print('Eh maior de idade')
+else:
+    print ('Eh menor de idade')
+
+#if (variavel 'Sinal' variavel)
+# é igual/diferente?
+#igual: ==
+# Diferente: !=
+#é maior/menos?
+
+
