bec1a1add32fe8b839e65b2047bd1c170b4848c2
Pamela Cruz 🖳 <117556345+pmlcrz@users.noreply.github.com>
2024-08-05 10:58:55 -0300

    Update atividade-revisada.js

--- MODULO1/Aula 7/atividade-revisada.js
+++ MODULO1/Aula 7/atividade-revisada.js
@@ -1,5 +1,20 @@
 nome = "Marcio";
 cpf = "145.678.910-11";
 endereco = "Rua vasco da gama";
+taxa = false;
 
-console.log("Olá, "+ nome +"!");
+console.log("Olá, " + nome + "!");
+
+if (endereco == "RJ"){
+    console.log("Eu, " + nome + ", portador do cpf" + cpf +", residente no endereco" 
+    + endereco + ", declaro que as minhas informações são verdadeiras.");
+
+} else {
+    if (taxa == true){
+        console.log("Eu, " + nome + ", portador do cpf" + cpf +", residente no endereco" 
+        + endereco + ", declaro que as minhas informações são verdadeiras.");
+
+    }else{
+        console.log("Por favor, pague a taxa");
+    }
+}
