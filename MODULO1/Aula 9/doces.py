ac060917c4d37ad62a9cb726478fd1e7f52c5ab0
Pamela Cruz 🖳 <117556345+pmlcrz@users.noreply.github.com>
2024-08-09 12:00:16 -0300

    Atividades aula 9

---
+++ MODULO1/Aula 9/doces.py
@@ -0,0 +1,13 @@
+doces = []
+
+doces.append('Pudim')
+doces.append('Brigadeiro')
+doces.append('Pave')
+doces.append('Quindim')
+doces.append('Torta de limao')
+
+print('Imprimindo a lista')
+print(doces)
+
+for doces in doces:
+    print(doces)
