0d00fa1ea37bb3a8a7868b89f7610318f60769ef
Pamela Cruz 🖳 <117556345+pmlcrz@users.noreply.github.com>
2024-08-14 12:01:32 -0300

    Update banco.py

--- MODULO1/Aula 10/banco.py
+++ MODULO1/Aula 10/banco.py
@@ -1,29 +1,47 @@
 class Conta:
     
-    def__init__(self, numeroConta, usuario, cpf, saldo = 0): # type: ignore
-    self.saldo = saldo
-    self.numeroConta = numeroConta
-    self.titular = usuario
-    self.cpf = cpf
+    def __init__(self, numeroConta, usuario, cpf, saldo = 0):
+        self.saldo = saldo
+        self.numeroConta = numeroConta
+        self.titular = usuario
+        self.cpf = cpf
     
     
-    def saque(delf, valor):
+    def saque(self, valor):
         if valor <= 0:
             print('Valor incompatível com a ação')
         elif self.saldo >= valor:
-             self.saldo = self.saldo - valor
-             
-             print('Saque realixado com sucesso')
+            self.saldo = self.saldo - valor
+            print('Saque realixado com sucesso')
         else:
             print('saldo insuficiente')
             
-    #def deposito(conta, valor):
-    
-    
-    conta1 = Conta('1234', 'Fulana', '987654321-00', 100)
-    
-    print(conta1.titular, conta1.saldo)
-    
-    conta1.saque(30)
-    
-    print(conta1.titular, conta1.saldo)
+    def deposito(self, conta, valor):
+        if valor <= 0:
+            print('insira valor valido para deposito')        
+        else: 
+            conta.saldo = conta.saldo + valor
+            print('Deposito feito com sucesso')
+        
+        
+    def transferencia(self, destino, valor):
+        self.saque(valor)
+        self.deposito(destino, valor)
+        print('transferencia feita com sucesso')
+        
+        
+conta1 = Conta('1234', 'Fulana', '987654321-00', 100)
+conta2 = Conta('1234', 'Fulana', '987654321-00', 30)
+conta1.transferencia(conta2, 30)
+print('Saldo após transferencia da conta 1:', conta1.saldo)
+print('Saldo após transferencia da conta 2:', conta2.saldo)
+        
+        
+
+print(conta2.titular, conta2.saldo)
+conta1.deposito(conta2, 0)
+print(conta2.titular, conta2.saldo)
+
+#conta.saque(30)
+
+#print(conta1.titular, conta1.saldo)
